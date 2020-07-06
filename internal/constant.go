1d5d16558451ad75fca24dc25a1dea6fab6cb9e5
WeAreThe99 <ivotelive@protonmail.com>
2020-07-06 16:55:10 +0300

    Update Start Text

--- internal/constant.go
+++ internal/constant.go
@@ -6,4 +6,6 @@ const BotTokenFlag = "token"
 const StartText = "Бот для генерации мемов про Лукашенку, введите @SymbalBot и текст мема, " +
 	"например: `@SymbalBot Я дрожащими руками//за трон не держусь`\n\n" +
 	"Две черты // разобьют текст мема на две строки: верхнюю и нижнюю\n\n" +
+	"Добавлять новые МЕМы можно через Pull Request на GitHub (https://github.com/wearethe99/meme). " +
+	"И помните: Народ и партия едины!" +
 	"2020 © «Пятая колонна» ivotelive@protonmail.com"
